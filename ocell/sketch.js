3a8cf715bc8ffb68b5ddf56ee57fdf3611888253
jbarriga4 <jbarriga4@iespompeufabra.cat>
2024-09-18 11:14:52 +0200

    Update sketch.js

--- ocell/sketch.js
+++ ocell/sketch.js
@@ -1,7 +1,35 @@
 function setup() {
- // put setup code here
+ createcanvas(480,120
 }
 
 function draw() {
-  // put drawing code here
+  background(204);
+ //ocell esquerra
+ beginshape();
+ vertex(50,120);
+  vertex(100,90);
+  vertex(110,60);
+  vertex(80,20);
+  vertex(220,60);
+  vertex(160,80);
+  vertex(200,90);
+  vertex(140,100);
+  vertex(130,120);
+ endshape();
+ fill(0);
+ ellipse(155,60,8,8)
+ //ocell de la dreta
+ beginshape();
+ vertex(120,50);
+  vertex(90,100);
+  vertex(60,110);
+  vertex(20,80);
+  vertex(60,220);
+  vertex(80,160);
+  vertex(90,200);
+  vertex(100,140);
+  vertex(120,130);
+ endshape();
+ fill(0);
+ ellipse(155,60,8,8)
 }
