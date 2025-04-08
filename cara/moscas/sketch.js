f850a1d1fe442a8a2b250ddd1af9bc9b5f48b37c
jbarriga4 <jbarriga4@iespompeufabra.cat>
2025-04-08 09:20:30 +0200

    Update sketch.js

--- cara/moscas/sketch.js
+++ cara/moscas/sketch.js
@@ -1,1 +1,45 @@
+let ales
+let ales2 
 
+
+function setup(){
+
+
+}
+function mosca(){
+    ales = 10*random(100)
+    ales2 = 10*random(100)
+    fill(0,0,0);
+    ellipse(50+ales2,50+ales,15,40);
+    fill(255,255,255);
+    ellipse(65+ales2,50+ales,15,65);
+    ellipse(35+ales2,50+ales,15,65);
+}
+
+function draw(){
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+  mosca()
+
+//highlight
