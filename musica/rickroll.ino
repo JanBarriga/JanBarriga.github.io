acd36a65eaef1c79bb14743a00696bffebdbd26e
jbarriga4 <jbarriga4@iespompeufabra.cat>
2025-05-15 09:14:47 +0200

    Create rickroll.ino
    matenme

---
+++ musica/rickroll.ino
@@ -0,0 +1,49 @@
+
+void setup() {
+  
+}
+
+void loop() {
+  //Never gonna give you up
+  tone(4, 311.127);
+  delay(100);
+  tone(4, 415.305);
+  delay(100);
+  tone(4, 349.228);
+  delay(100);
+  tone(466.164);
+  delay(100);
+  tone(4, 311.127);
+  delay(100);
+  tone(4, 415.305);
+  delay(100);
+  tone(4, 349.228);
+  delay(100);
+  tone(4, 466.164);
+  delay(100);
+  //a
+  tone(4, 311.127);
+  delay(100);
+  tone(4, 415.305);
+  delay(100);
+  tone(4, 349.228);
+  delay(50);
+  tone(4, 466.164);
+  delay(50);
+  tone(4, 277.183);
+  delay(100);
+  tone(4, 369.994);
+  delay(100);
+  tone(4, 369.994);
+  delay(100);
+  tone(4, 415.306);
+  delay(400);
+  tone(4, 369.994);
+  delay(100);
+  tone(4, 311.127);
+  delay(100);
+  tone(4, 415.305);
+  delay(100);
+  tone(466.164);
+  delay(100);
+}
